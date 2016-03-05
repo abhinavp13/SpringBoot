350bbe796b7a27c2cca3809e6097acf2354a02f2
abhinavp13 <pabhinav@iitrpr.ac.in>
2016-03-06 01:30:54 +0530

    Added Lombok dependency in RestfulWebService and initialized Detecting Device Type project

--- RestfulWebService/src/main/java/com/pabhinav/Greeting.java
+++ RestfulWebService/src/main/java/com/pabhinav/Greeting.java
@@ -1,24 +1,21 @@
 package com.pabhinav;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+
 /**
  * @author pabhinav
  */
 public class Greeting {
 
+    @Getter(AccessLevel.PUBLIC)
     private final long id;
 
+    @Getter(AccessLevel.PUBLIC)
     private final String content;
 
     public Greeting(long id, String content){
         this.id  = id;
         this.content = content;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
     }
 }
